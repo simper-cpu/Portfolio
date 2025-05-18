bf0222cece1c7b1ca37976c426e145ce9789303c
Mai Phú Huy <24280014@student.hcmus.edu.vn>
2025-05-18 16:21:33 +0700

    lol

--- app/data.ts
+++ app/data.ts
@@ -27,16 +27,16 @@ type SocialLink = {
   link: string
 }
 
-export const EMAIL = ''
+export const EMAIL = 'simp@gmail.com'
 
-export const WEB_TITLE = 'Name - Personal Website'
+export const WEB_TITLE = 'KoAiSimpBangToi - Personal Website'
 export const WEB_DESCRIPTION =
   'A personal website showcasing my work and projects.'
-export const FOOTER_COPYRIGHT = '© 2025 Nguyen Van B.'
+export const FOOTER_COPYRIGHT = '© 2025 KoAiSimpBangToi.'
 export const FOOTER_LINK = 'https://github.com/username/'
 
-export const NAME = 'Mai Phu Huy'
-export const JOB_TITLE = 'Frontend intern'
+export const NAME = 'KoAiSimpBangToi'
+export const JOB_TITLE = 'DevOps intern'
 export const DESCRIPTION =
   'I am a passionate designer and developer with a focus on creating beautiful and functional user interfaces. I have experience in design engineering, front-end development, and product design. I love to explore the intersection of design and technology, and I am always looking for new challenges to tackle.'
 
